de7f66d6670d3f169187101f9a06c793a7729eb0
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2022-02-01 13:49:30 -0500

    Add project files.

---
+++ Video_100/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnShowMsg = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnShowMsg
+        '
+        Me.btnShowMsg.Location = New System.Drawing.Point(77, 39)
+        Me.btnShowMsg.Name = "btnShowMsg"
+        Me.btnShowMsg.Size = New System.Drawing.Size(136, 23)
+        Me.btnShowMsg.TabIndex = 0
+        Me.btnShowMsg.Text = "Show Message"
+        Me.btnShowMsg.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(313, 154)
+        Me.Controls.Add(Me.btnShowMsg)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnShowMsg As Button
+End Class
---
+++ Video_100/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnShowMsg_Click(sender As Object, e As EventArgs) Handles btnShowMsg.Click
+        Using myDoStuff As New doStuff
+            myDoStuff.showMessage()
+            myDoStuff.showMoreMessages()
+        End Using
+    End Sub
+End Class
